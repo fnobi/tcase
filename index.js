3584ede20fb2fa94c1a4fcd9bf026869b3bcf02e
fnobi <shin@fnobi.com>
2012-11-20 13:12:23 +0900

    先祖関係も表した出力ができるようになった。

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
 var fs       = require('fs'),
-
-    ItemList = require(__dirname + '/lib/ItemList'),
-
-    async    = require('async'),
-    config   = require('config'),
     jade     = require('jade');
 
 var cheerio  = {
-      select : require('cheerio-select'),
-      parse  : require('cheerio')
+	select : require('cheerio-select'),
+	parse  : require('cheerio')
 };
 
-var TCase = function (initSource) {
-	this.source = '';
-	this.addSource(initSource);
+var ItemList = require(__dirname + '/lib/ItemList');
+
+
+var TCase = function (source) {
+	this.xml = this.compile(source);
 };
 
-TCase.prototype.addSource = function (source) {
-	source = source || '';
-	this.source += '\n' + source;
-};
-
-TCase.prototype.xmlSource = function () {
-	return jade.compile(this.source, {})({});
+TCase.prototype.compile = function (source) {
+	return jade.compile(source, {})({});
 };
 
 TCase.prototype.select = function (selector) {
-	var dom = cheerio.parse(this.xmlSource());
+	var dom = cheerio.parse(this.xml);
 	var filteredDom = cheerio.select(selector, dom);
 
 	return new ItemList(filteredDom);
 };
-
-TCase.sourceDir = config.sourceDir.replace(/^~/, process.env.HOME);
-
-TCase.sourceFiles = function (callback) {
-	var sourceFiles = [];
-
-	fs.readdir(TCase.sourceDir, function (err, res) {
-		res.forEach(function (sourceFile) {
-			if (sourceFile.match(/^[^.].*\.jade/)) {
-				sourceFiles.push(TCase.sourceDir + sourceFile);
-			}
-		});
-		callback(err, sourceFiles);
-	});
-};
-
 
 module.exports = TCase;
